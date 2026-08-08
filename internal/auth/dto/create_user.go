c740f5945a8bc2531b005dede92492c70de41a7d
Archange Malonga <malongaarchange242@gmail.com>
2026-08-08 11:38:09 +0100

    Mise à jour

--- internal/auth/dto/create_user.go
+++ internal/auth/dto/create_user.go
@@ -3,10 +3,7 @@ package dto
 type CreateUserRequest struct {
 	FirstName string `json:"first_name" binding:"required"`
 	LastName  string `json:"last_name" binding:"required"`
-	Email     string `json:"email" binding:"required,email"`
-	Password  string `json:"password" binding:"omitempty,min=8"`
-	Phone     string `json:"phone"`
-	Gender    string `json:"gender"`
+    Email     string `json:"email" binding:"omitempty,email"`
 	RoleID    int64  `json:"role_id" binding:"required"`
 	StationID *int64 `json:"station_id"`
 }
