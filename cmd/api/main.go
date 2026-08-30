635c553304bde8c2812eea7df8d583b4f10259d8
malongaarchange242-Arki <malongaarchange242@gmail.com>
2026-08-30 12:16:26 +0100

    Fix my file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -294,14 +294,14 @@ func main() {
 		c.Header("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 		c.Header("Cross-Origin-Opener-Policy", "same-origin")
 		c.Header("Cross-Origin-Embedder-Policy", "require-corp")
-		c.Header("Cross-Origin-Resource-Policy", "same-origin")
+		c.Header("Cross-Origin-Resource-Policy", "cross-origin")
 		c.Header("Content-Security-Policy",
 			"default-src 'self'; "+
 				"script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; "+
 				"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "+
 				"img-src 'self' data: https:; "+
 				"font-src 'self' data: https://fonts.gstatic.com; "+
-				"connect-src 'self' https://lunetterie-frontend.onrender.com; "+
+				"connect-src 'self' https://api-lunetterie.universearch.com https://lunetterie-front.onrender.com http://localhost:8000 http://127.0.0.1:8000; "+
 				"frame-ancestors 'none'; "+
 				"base-uri 'self'; "+
 				"form-action 'self'")
@@ -314,8 +314,10 @@ func main() {
 			"https://www.api-lunetterie.universearch.com",
 			"http://localhost:3000",
 			"http://localhost:8080",
+			"http://localhost:8000",
 			"http://127.0.0.1:5501",
 			"http://127.0.0.1:5500",
+			"http://127.0.0.1:8000",
 			"http://localhost:8443",
 			"http://127.0.0.1:8080",
 			"http://localhost:8443",
